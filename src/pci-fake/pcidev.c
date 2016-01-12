f8457c5aa33466a1a456c95f0e60f70530b7657c
john <cpp.cheen@gmail.com>
2016-01-12 12:47:05 +0800

    save

--- src/pci-fake/pcidev.c
+++ src/pci-fake/pcidev.c
@@ -1,4 +1,4 @@
-
+#include <linux/err.h>
 #include <linux/types.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -13,6 +13,8 @@
 
 
 
+
+#define     MAX_DEVICE      4
 #define     PCI_DEVICE_NAME     "fake-pci"      /* will show in '/proc/devices' */
 #define		DEBUG_TAG          " LDD-DEBUG "
 
@@ -21,12 +23,14 @@
 #define SSX10A_VENDER_ID    0x0922
 #define SSX10A_DEVICE_ID    0x0100
 
+s
 
 MODULE_DESCRIPTION("FAKE PCI  driver");
 MODULE_AUTHOR("john (cpp.cheen@gmail.com)");
 MODULE_LICENSE("GPL");
 
 
+static dev_t devno;
 
 static struct pci_device_id pci_ids[] = {
 	{ PCI_DEVICE(SSX10A_VENDER_ID, SSX10A_DEVICE_ID), },
@@ -70,16 +74,31 @@ static struct pci_driver pci_driver = {
 	.remove = remove,
 };
 
-static int __init pci_skel_init(void)
+static int __init krn_module_init(void)
 {
-	return pci_register_driver(&pci_driver);
+	int ret_code;
+    ret_code= alloc_chrdev_region(&devno,0,MAX_DEVICE,PCI_DEVICE_NAME);//$cat /proc/devices
+    if (ret_code < 0) {
+		printk(KERN_ERR DEBUG_TAG "can't get major\n");
+		return ret_code;
+	}
+	ret_code = pci_register_driver(&pci_driver);
+	if (ret_code < 0) {
+		printk(KERN_ERR DEBUG_TAG "pci-driver: can't register pci driver\n");
+        //class_destroy(dev_class);
+        unregister_chrdev_region(devno, MAX_DEVICE);
+
+	}
+	return ret_code
+	//return pci_register_driver(&pci_driver);
 }
 
-static void __exit pci_skel_exit(void)
+static void __exit krn_module_exit(void)
 {
 	pci_unregister_driver(&pci_driver);
+    unregister_chrdev_region(devno, MAX_DEVICE);
 }
 
 
-module_init(pci_skel_init);
-module_exit(pci_skel_exit);
+module_init(krn_module_init);
+module_exit(krn_module_exit);
